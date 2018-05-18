694172a9a8bdf6dbd4a2924529c3f781aed696dc
vshulepov <vladyslav.shuliepov@uniquesoft.com>
2018-05-18 14:38:41 +0300

    Test added. GrammarGen.parse() extended

--- src/main/kotlin/com/blacksun/Main.kt
+++ src/main/kotlin/com/blacksun/Main.kt
@@ -1,20 +1,8 @@
 package com.blacksun
 
 import com.blacksun.utils.GrammarGen
-import com.blacksun.utils.Node
-import java.io.File
 
 fun main(args: Array<String>) {
     GrammarGen.initGrammar("grammar.gr")
-    val node = GrammarGen.parse(File("res/test.sig"))
-    val matchNode = Node("<signal-program>")
-    val program = matchNode + Node("<program>")
-    val any = Node("?")
-    program + any
-    program + Node("?ident")
-    program + any
-    program + any
-    program + any
-    val matcher = node.match(matchNode)
-    matcher["?ident"]?.print()
+    GrammarGen.parse(" 15..75", "<range>").print()
 }
